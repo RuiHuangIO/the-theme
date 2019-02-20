d008363186ed05075e2b785c324a4b95438dff8b
HR <hrui1990@gmail.com>
2019-02-19 18:48:34 -0600

    bruh webpack sucks man

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,42 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: ["./src/js/main.js", "./src/sass/the-theme.scss"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  devtool: "source-map",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+      chunkFilename: "[name].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader"
+          },
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+};
