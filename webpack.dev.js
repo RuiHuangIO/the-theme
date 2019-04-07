e460e9f789206cf1af288921ace3f6acd7a73782
HR <hrui1990@gmail.com>
2019-04-07 01:17:44 -0500

    oops forgot to lint webpack config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,32 +1,32 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const common = require('./webpack.common');
+const path = require("path");
+const merge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: "development",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html"
     }),
-    new MiniCssExtractPlugin({ filename: '[name].css' }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
     new BrowserSyncPlugin(
       {
-        host: 'localhost',
+        host: "localhost",
         port: 3000,
-        proxy: 'http://localhost:8080/',
+        proxy: "http://localhost:8080/"
       },
       {
-        reload: false,
+        reload: false
       }
-    ),
+    )
   ],
   module: {
     rules: [
@@ -35,15 +35,15 @@ module.exports = merge(common, {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
-            options: { sourceMap: true },
+            loader: "css-loader",
+            options: { sourceMap: true }
           },
           {
-            loader: 'sass-loader',
-            options: { sourceMap: true },
-          },
-        ],
-      },
-    ],
-  },
+            loader: "sass-loader",
+            options: { sourceMap: true }
+          }
+        ]
+      }
+    ]
+  }
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,46 +1,46 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const merge = require('webpack-merge');
-const common = require('./webpack.common');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin"); // eslint-disable-line import/no-extraneous-dependencies
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contentHash].bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contentHash].bundle.js"
   },
   optimization: {
     minimizer: [
       new OptimizeCssAssetsPlugin(),
       new TerserPlugin(),
       new HtmlWebpackPlugin({
-        template: './src/index.html',
+        template: "./src/index.html",
         minify: {
           removeAttributeQuotes: true,
           collapseWhitespace: true,
-          removeComments: true,
-        },
-      }),
-    ],
+          removeComments: true
+        }
+      })
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].[contentHash].css',
-      chunkFilename: '[name].[contentHash].css',
+      filename: "[name].[contentHash].css",
+      chunkFilename: "[name].[contentHash].css"
     }),
 
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  }
 });
